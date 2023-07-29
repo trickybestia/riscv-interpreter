493968cf2ee0aa700ec322c36a394187e9693379
trickybestia <trickybestia@gmail.com>
2023-07-29 15:16:04 +0300

    Simplify development with VS Code

--- example/build.sh
+++ example/build.sh
@@ -4,19 +4,21 @@ mkdir -p build
 
 echo "*" > build/.gitignore
 
+BUILDDIR="build"
+
+BEAR="/usr/bin/bear"
 CLANG="/usr/bin/clang"
 LD="/usr/bin/ld.lld"
 OBJCOPY="/usr/bin/llvm-objcopy"
 
-CFLAGS="$CFLAGS --target=riscv32 -march=rv32i -nostdlib"
+CFLAGS="$CFLAGS --target=riscv32 -march=rv32i -nostdlib -std=c17 -Os"
 LDFLAGS="$LDFLAGS -Tlink.ld"
 
-BUILDDIR="build"
+COMPILE="$BEAR --append --output $BUILDDIR/compile_commands.json -- $CLANG $CFLAGS"
 
-$CLANG $CFLAGS src/main.c -S -o "$BUILDDIR"/main.s
-$CLANG $CFLAGS src/_start.s -c -o "$BUILDDIR"/_start.o
-$CLANG $CFLAGS src/syscall.s -c -o "$BUILDDIR"/syscall.o
-$CLANG $CFLAGS src/main.c -c -o "$BUILDDIR"/main.o
+$COMPILE src/_start.s -c -o "$BUILDDIR"/_start.o
+$COMPILE src/syscall.s -c -o "$BUILDDIR"/syscall.o
+$COMPILE src/main.c -c -o "$BUILDDIR"/main.o
 
 $LD $LDFLAGS -o "$BUILDDIR"/example.o "$BUILDDIR"/_start.o "$BUILDDIR"/syscall.o "$BUILDDIR"/main.o
 
